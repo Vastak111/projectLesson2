81cb59ddd1cb2a276bd3bff72bf9378891d0f0ba
Valera <vastak2009@rambler.ru>
2022-06-14 00:38:53 +0300

    Created project for homework

---
+++ src/robots/DancingRobot.java
@@ -0,0 +1,13 @@
+package robots;
+
+public abstract class DancingRobot {
+
+    public String getDefaultDanceType() {
+        return getDanceType1();
+    }
+
+    public abstract String getDanceType1();
+
+    public abstract String getDanceType2();
+
+}
