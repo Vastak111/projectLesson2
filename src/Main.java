cf94a10fd27aa5c28ea649c525f5577e3f89ea57
Valera <vastak2009@rambler.ru>
2022-06-18 23:20:47 +0300

    First part of homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import robots.Robot1;
+import robots.Robot2;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Robot1 robot1 = new Robot1();
+        Class<Robot1> robot1info = Robot1.class;
+
+        Robot2 robot2 = new Robot2();
+        Class<Robot2> robot2info = Robot2.class;
+
+        System.out.println("Robot from class " + robot1info.getName()+ " Can dance " + robot1.getDanceType1());
+        System.out.println("Robot from class " + robot2info.getName() + " Can dance " + robot2.getDanceType1());
     }
 }
--- src/robots/Robot2.java
+++ src/robots/Robot2.java
@@ -1,5 +1,16 @@
 package robots;
 
-public class Robot2 {
+public class Robot2 extends DancingRobot{
 
+    private final String roboDancing1 = "Ballet";
+    private final String RoboDancing2 = "Folk Dance";
+    @Override
+    public String getDanceType1() {
+        return this.roboDancing1;
+    }
+
+    @Override
+    public String getDanceType2() {
+        return this.RoboDancing2;
+    }
 }
